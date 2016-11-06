edebc471bda0d1e63727e95d9bc51a5725d64147
ALIEN <matthias.moulin@gmail.com>
2016-11-06 20:42:14 +0100

    Updated documentation

--- Direct3D 11/Project 3/Project 3/src/geometry.hpp
+++ Direct3D 11/Project 3/Project 3/src/geometry.hpp
@@ -24,18 +24,18 @@ public:
 	/**
 	 Constructs a vertex.
 	 */
-	Vertex() : p(DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f)) {}
+	Vertex() : p(XMFLOAT3(0.0f, 0.0f, 0.0f)) {}
 
 	/**
 	 Constructs a vertex.
 
 	 @param[in]		p
-					Position of the vertex (in world space).
+					Position of the vertex (in projection space).
 	 */
 	Vertex(XMFLOAT3 p) : p(p) {}
 
 	/**
-	 Position of this vertex (in world space).
+	 Position of this vertex (in projection space).
 	 */
 	XMFLOAT3 p;
 };
@@ -48,7 +48,7 @@ public:
 // 6. Identifies the input data class for a single input slot.
 // 7. The number of instances to draw using the same per instance data before advancing in the buffer by one element.
 const D3D11_INPUT_ELEMENT_DESC vertex_input_element_desc[] = {
-	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, UINT(offsetof(Vertex, p)), D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, UINT(offsetof(Vertex, p)), D3D11_INPUT_PER_VERTEX_DATA, 0 }
 };
 
 #pragma endregion
